dab6bd0b8d9167f36ee132172f22cd8a7fe3bfae
anandrajB <anand98.ar@gmail.com>
2022-04-29 23:20:41 +0530

    v2

--- day2/accounts/admin.py
+++ day2/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Principal , School , student , Staff
 
 # Register your models here.
+admin.site.register(Principal)
+admin.site.register(student)
+admin.site.register(Staff)
+admin.site.register(School)
--- day2/accounts/models.py
+++ day2/accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=155)
+    address = models.CharField(max_length=155)
+    city = models.CharField(max_length=155)
+    state = models.CharField(max_length=155)
+    country = models.CharField(max_length=155)
+
+
+class Principal(models.Model):
+    school_id = models.ForeignKey(School,on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=155)
+    address = models.CharField(max_length=155)
+    salary = models.CharField(max_length=155)
+    joined_date = models.DateTimeField(auto_now_add=True)
+    
+
+
+class Staff(models.Model):
+    first_name = models.CharField(max_length=155)
+    last_name = models.CharField(max_length=155)
+    classroom_id = models.IntegerField()
+    is_class_councellor = models.BooleanField(default=False)
+    subject = models.CharField(max_length=155)
+
+
+class student(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_class_leader = models.BooleanField(default=False)
+    standard = models.CharField(max_length=255)
+    staff_id = models.ForeignKey(Staff,on_delete=models.CASCADE)
